b6f3d79107762be1ce6191bd11f2504b91316607
GangHyeon An <cccloud1016@gmail.com>
2024-11-05 11:22:34 +0900

    2024.11.05[Create] [SELECT option/from~ where]

--- 2장SQL.sql
+++ 2장SQL.sql
@@ -115,3 +115,37 @@ INSERT INTO buy VALUES(NULL, 'MMU', '지갑', NULL, 30, 4);
 SELECT * FROM member;
 SELECT * FROM buy;
 
+SELECT mem_name FROM member;	
+SELECT mem_name AS '볣' FROM member;
+
+SELECT addr AS 주소, debut_date AS 데뷔일자 , mem_name AS 멤버이름 
+	FROM member 
+	WHERE height=162;
+
+SELECT * 
+	FROM member 
+	WHERE mem_name='블랙핑크';
+
+SELECT * 
+	FROM member
+	WHERE mem_number=4;
+    
+SELECT mem_name, height, mem_number 
+	FROM member 
+	WHERE height >= 165 AND mem_number > 6;
+
+SELECT mem_name, height, mem_number
+	FROM member
+    WHERE height BETWEEN 163 AND 165;
+
+SELECT mem_name, addr
+	FROM member
+    WHERE addr IN('경기', '전남', '경남');
+    
+SELECT mem_name, addr
+	FROM member
+    WHERE addr IN('경', '전', '경');
+    
+SELECT * 
+	FROM member
+    WHERE mem_name LIKE '__핑_';
