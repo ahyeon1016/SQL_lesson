24f30150493f1543996754b84ff24be92b32e906
GangHyeon An <cccloud1016@gmail.com>
2024-11-11 09:49:53 +0900

    2024.11.11[Create] [TRIGGER]

--- 7장SQL.sql
+++ 7장SQL.sql
@@ -102,3 +102,33 @@ DELIMITER ;
 
 DROP PROCEDURE dynamic_proc;
 CALL dynamic_proc('member');
+
+-- TRIGGER
+
+USE market_db;
+CREATE TABLE IF NOT EXISTS trigger_table(
+	id INT,
+    txt VARCHAR(10)
+);
+INSERT INTO trigger_table VALUES(1, '레드벨벳'); 
+INSERT INTO trigger_table VALUES(2, '잇지'); 
+INSERT INTO trigger_table VALUES(3, '블랙핑크'); 
+SELECT * FROM trigger_table;
+
+DELIMITER $$
+CREATE TRIGGER myTrigger
+	AFTER DELETE
+    ON trigger_table
+    FOR EACH ROW
+BEGIN
+	SET @msg = '가수 그룹이 삭제됨';
+END $$
+DELIMITER ;
+
+SET @msg = '';
+INSERT INTO trigger_table VALUES(4, '마마무');
+SELECT @msg;
+UPDATE trigger_table SET txt = '블핑' WHERE id=3;
+SELECT @msg;
+DELETE FROM trigger_table WHERE id=4;
+SELECT @msg; 
