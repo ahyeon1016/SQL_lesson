71962e17949832662633d548d89906c98e4f2aec
GangHyeon An <cccloud1016@gmail.com>
2024-11-11 10:15:16 +0900

    2024.11.11[Create] [TRIGGER-BACKUP]

--- 7장SQL.sql
+++ 7장SQL.sql
@@ -132,3 +132,42 @@ UPDATE trigger_table SET txt = '블핑' WHERE id=3;
 SELECT @msg;
 DELETE FROM trigger_table WHERE id=4;
 SELECT @msg; 
+
+
+CREATE TABLE singer(SELECT mem_id, mem_name, mem_number, addr FROM member);
+SELECT * FROM singer;
+
+CREATE TABLE backup_singer(
+	mem_id CHAR(8) NOT NULL,
+    mem_name VARCHAR(10) NOT NULL,
+    mem_number INT NOT NULL,
+    addr CHAR(2) NOT NULL,
+    modType CHAR(2),
+    modDate DATE,
+    modUser VARCHAR(30)
+); 
+
+DELIMITER $$
+CREATE TRIGGER singer_updateTrg
+	AFTER UPDATE
+    ON singer
+    FOR EACH ROW
+BEGIN
+	INSERT INTO backup_singer VALUES(OLD.mem_id, OLD.mem_name, OLD.mem_number, OLD.addr, '수정', CURDATE(), CURRENT_USER());
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER singer_deleteTrg
+	AFTER DELETE
+    ON singer
+    FOR EACH ROW
+BEGIN
+	INSERT INTO backup_singer VALUES(OLD.mem_id, OLD.mem_name, OLD.mem_number, OLD.addr, '삭제', CURDATE(), CURRENT_USER());
+END $$
+DELIMITER ;
+
+UPDATE singer SET addr='영국' WHERE mem_id='BLK';
+DELETE FROM singer WHERE mem_number >= 7;
+
+SELECT * FROM backup_singer;
